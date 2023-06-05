eee041898bd87fdbfa04e7a0fd4ebebb01bc577c
songshuhan <slof-songshuhan@163.com>
2023-06-05 13:02:00 +0800

    line main

--- new_work.py
+++ new_work.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 # ldoakjsifkljaskldfjlaksjdf
 #dhfaeuih1qjbncvcn
 #21323432321321321
 # 877
 
-=======
-# revise 1 rh
-# add some on branch fb
-# fix bug on fb
->>>>>>> 564da0f (fix fb bug)
