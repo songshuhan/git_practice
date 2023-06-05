aa9db1a13d85d27963dc32db48ba3a1cfd520045
songshuhan <slof-songshuhan@163.com>
2023-06-05 12:45:03 +0800

    merge?

--- new_work.py
+++ new_work.py
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 # ldoakjsifkljaskldfjlaksjdf
 #dhfaeuih1qjbncvcn
 #21323432321321321
 # 877
 
+=======
+# revise 1 rh
+# add some on branch fb
+# fix bug on fb
+>>>>>>> 564da0f (fix fb bug)
